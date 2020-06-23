ef6d3c3d16117fa0f4d157a622784bf3464fb9c3
Anthony Pillot <pillot.anthony@gmail.com>
2020-06-23 23:58:09 +0200

    feat: add pages, logo and photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <span>Here is the home page.</span>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
