08f95f6ed3de4307b895896661e537eb678f5660
Anthony Pillot <pillot.anthony@gmail.com>
2020-06-30 15:21:21 +0200

    feat: add texts, responsive and minor typos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <h1>Bienvenue sur mon site internet personnel.</h1>
-
     <v-carousel cycle height="350" show-arrows-on-hover interval="5000">
       <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
         <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3">{{ slide.text }}</div>
+          <div class="display-2 hidden-sm-and-down">{{ slide.text }}</div>
         </v-row>
       </v-carousel-item>
     </v-carousel>
@@ -13,21 +11,28 @@
     <v-divider class="my-4"></v-divider>
 
     <h1>Présentation :</h1>
-    <span>
-      Ayant commencé sur Java, je travaille aujourd'hui dans l'écosystème
-      JavaScript, avec Node.js et Vue.js.
-    </span>
-    <br />
-    <span>SQL avec PostgreSQL, NoSQL avec MongoDB.</span>
-    <v-spacer />
+    <p>
+      Qui suis-je ? Anthony Pillot, et il y a certaine chose que j'adore dans la
+      vie : la high technology, ma passion depuis toujours. Amoureux d'Apple, de
+      Windows (c'est possible !) de Linux, avec mon serveur personnel sous
+      Debian, Ubuntu en Desktop et des applications IT quelles qu'elles soient !
+    </p>
+    <p>
+      Ayant commencé sur Java, JEE et son écosystème, je travaille aujourd'hui
+      dans l'écosystème JavaScript, TypeScript, ainsi que Node.js et Vue.js.
+    </p>
+    <p>
+      Je travaille également sur la conception de base de données en SQL avec
+      PostgreSQL, Microsoft SQL Server, en NoSQL avec MongoDB.
+    </p>
 
     <v-divider class="my-4"></v-divider>
 
     <h2>Compétences clés :</h2>
-    <span>
-      Travail en équipe. Avenant. Passionné. Autodidacte. Autonome.
-      Anticipation. Organisation d'une équipe de travail. Veille technologique.
-    </span>
+    <p>
+      Avenant. Passionné. Autodidacte. Autonome. Anticipation. Organisation
+      d'une équipe de travail. Veille technologique.
+    </p>
   </div>
 </template>
 
@@ -53,12 +58,12 @@ export default {
           "https://images.idgesg.net/images/article/2019/02/certification_school_pencils_diversity_creative_team_by-evgeny555-getty-100787956-large.jpg",
       },
       {
-        text: "",
+        text: "Dark mode available",
         src:
           "https://image.winudf.com/v2/image1/Y29tLnJpYmJ5dGUuZGFya21vZGUuZmIucHJvX3NjcmVlbl8wXzE1NjUyOTUwNzlfMDMx/screen-0.jpg?fakeurl=1&type=.jpg",
       },
       {
-        text: "Working on multiple languages and technologies",
+        text: "Working on multiple languages",
         src:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
       },
