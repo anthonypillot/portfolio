a0e6fe02058e7668ef9e6290f1fffd613555924d
Anthony Pillot <pillot.anthony@gmail.com>
2020-07-03 17:08:49 +0200

    style: space on top of v-card added

--- src/views/UnderConstruction.vue
+++ src/views/UnderConstruction.vue
@@ -1,17 +1,21 @@
 <template>
   <v-container fluid>
-    <v-card class="mx-auto" outlined tile>
-      <v-img
-        class="align-center text-center"
-        height="200px"
-        src="https://picsum.photos/1920/1080?random"
-      >
-        <h2>Cette page est en contruction</h2>
-        <span>
-          Revenez plus tard pour voir les nouveautés
-        </span>
-      </v-img>
-    </v-card>
+    <v-col cols="12">
+      <v-row align="center" justify="center" class="" style="height: 400px;">
+        <v-card outlined tile>
+          <v-img
+            class="align-center text-center"
+            height="300px"
+            src="https://picsum.photos/1920/1080?random"
+          >
+            <h2>Cette page est en contruction</h2>
+            <span>
+              Revenez plus tard pour voir les nouveautés
+            </span>
+          </v-img>
+        </v-card>
+      </v-row>
+    </v-col>
   </v-container>
 </template>
 
