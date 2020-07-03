a4d3a0a7a25008a209597b8e570619f8d04d8403
Anthony Pillot <pillot.anthony@gmail.com>
2020-07-03 14:28:20 +0200

    feat: add parallax in under-construction page

--- src/views/UnderConstruction.vue
+++ src/views/UnderConstruction.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <h2>
-      Cette page est en construction.
-    </h2>
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-4 font-weight-thin mb-4 black--text">
+            Under Construction
+          </h1>
+          <span class="black--text"
+            >We're working hard on it ! Come back later.</span
+          >
+        </v-col>
+      </v-row>
+    </v-parallax>
   </div>
 </template>
 
@@ -12,4 +21,4 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped></style>
