48d58c6cb170829cc9bac7d02b35095316470551
Anthony Pillot <pillot.anthony@gmail.com>
2020-07-09 17:20:00 +0200

    test: init cypress with some e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,51 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Root page test", () => {
+  it("Visits and tests the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("Anthony PILLOT");
+    cy.contains("Consultant Développeur Informatique");
+    cy.get(".align-center > .v-image > .v-responsive__content");
+    cy.contains("© " + new Date().getFullYear());
+    cy.contains("All rights reserved.");
+    cy.contains("Application version:");
+  });
+  it("Tests dark mode", () => {
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.get(".hidden-xs-only > .row > .v-btn").click();
+    cy.get(".v-overlay__scrim").click();
+    cy.visit("/");
+  });
+  it("Visits and tests navigation app", () => {
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("MENU PRINCIPAL");
+    cy.contains("Accueil").click();
+
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("Expérience").click();
+    cy.url().should("include", "/experience");
+
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("Projets").click();
+    cy.url().should("include", "/under-construction");
+
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("Blog").click();
+    cy.url().should("include", "/under-construction");
+
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("Contact").click();
+    cy.url().should("include", "/under-construction");
+
+    cy.visit("/");
+    cy.get(".v-app-bar__nav-icon").click();
+    cy.contains("À propos").click();
+    cy.url().should("include", "/about");
   });
 });
