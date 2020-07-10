743446249f8c796978a4452e479666fd1c423ebe
Anthony Pillot <pillot.anthony@gmail.com>
2020-07-10 13:56:00 +0200

    ci: create GitHub Actions CI using Cypress

--- tests/e2e/specs/global-test.js
+++ tests/e2e/specs/global-test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("Root page test", () => {
+describe("Global tests on root page", () => {
   it("Visits and tests the app root url", () => {
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.contains("Anthony PILLOT");
     cy.contains("Consultant Développeur Informatique");
     cy.get(".align-center > .v-image > .v-responsive__content");
@@ -11,39 +11,39 @@ describe("Root page test", () => {
     cy.contains("Application version:");
   });
   it("Tests dark mode", () => {
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.get(".hidden-xs-only > .row > .v-btn").click();
     cy.get(".v-overlay__scrim").click();
     cy.visit("/");
   });
   it("Visits and tests navigation app", () => {
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("MENU PRINCIPAL");
     cy.contains("Accueil").click();
 
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("Expérience").click();
     cy.url().should("include", "/experience");
 
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("Projets").click();
     cy.url().should("include", "/under-construction");
 
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("Blog").click();
     cy.url().should("include", "/under-construction");
 
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("Contact").click();
     cy.url().should("include", "/under-construction");
 
-    cy.visit("/");
+    cy.visit("http://localhost:8080");
     cy.get(".v-app-bar__nav-icon").click();
     cy.contains("À propos").click();
     cy.url().should("include", "/about");
