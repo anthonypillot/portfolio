7c057104649e1f6b0425d3e5e43e1e6121abdd24
Anthony Pillot <pillot.anthony@gmail.com>
2021-01-25 15:47:55 +0100

    ci: add docker run container on server step

--- data/docker-depoy-commands.sh
+++ data/docker-depoy-commands.sh
@@ -9,4 +9,4 @@ sudo docker run -d --name matomo -p 81:80 -p 82:443  --network website --link ma
 
 # Fourth step, create nginx container with personal website and link it to the previous created network
 # Letsencrypt certificates created previously on server side mounted in the nginx container with -v parameter
-sudo docker run -d --name nginx-website -p 80:80 -p 443:443 --network website -v /etc/letsencrypt/archive/www.anthony-pillot.com/:/etc/nginx/certs anthonypillot/website
+sudo docker run -d --name nginx-website -p 80:80 -p 443:443 -v /etc/letsencrypt/archive/www.anthony-pillot.com/:/etc/nginx/certs anthonypillot/website
