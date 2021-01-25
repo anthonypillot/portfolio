483d1fb8b6527a9f54eda78a7bbbcfa02077b4fb
Anthony Pillot <pillot.anthony@gmail.com>
2021-01-25 15:22:25 +0100

    build: update certificates location and move file in /data

---
+++ data/docker-depoy-commands.sh
@@ -0,0 +1,12 @@
+# First step, need to create Docker network to join all containers together
+sudo docker network create --driver=bridge website
+
+# Second step, create the MySQL database container and link it to the previous created network
+sudo docker run -d --name matomo-mysql --network website -e MYSQL_ROOT_PASSWORD= mysql:latest
+
+# Third step, create Matomo container and link it to the previous created network
+sudo docker run -d --name matomo -p 81:80 -p 82:443  --network website --link matomo-mysql:db matomo
+
+# Fourth step, create nginx container with personal website and link it to the previous created network
+# Letsencrypt certificates created previously on server side mounted in the nginx container with -v parameter
+sudo docker run -d --name nginx-website -p 80:80 -p 443:443 --network website -v /etc/letsencrypt/archive/www.anthony-pillot.com/:/etc/nginx/certs anthonypillot/website
