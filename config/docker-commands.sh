44b01723e567ab60350b007d02f83ed9140e7f86
Anthony Pillot <pillot.anthony@gmail.com>
2021-02-03 19:16:59 +0100

    feat: update docker command with --restart always

--- config/docker-commands.sh
+++ config/docker-commands.sh
@@ -5,12 +5,12 @@ docker network create --driver=bridge website
 docker run -d --name matomo-mysql --network website -e MYSQL_ROOT_PASSWORD= mysql:latest
 
 # Third step, create Matomo container and link it to the previous created network
-docker run -d --name matomo -p 81:80 -p 82:443  --network website --link matomo-mysql:db matomo
+docker run -d --name matomo -p 81:80 -p 82:443  --network website matomo
 
 # Fourth step, create nginx container with personal website and link it to the previous created network
 # Letsencrypt certificates created previously on server side mounted in the nginx container with -v parameter
-docker run -d --name nginx-website -p 80:80 -p 443:443 -v /etc/letsencrypt/archive/anthony-pillot.com/:/etc/nginx/certs anthonypillot/website
+docker run -d --restart always --name nginx-website -p 80:80 -p 443:443 -v /etc/letsencrypt/archive/anthony-pillot.com/:/etc/nginx/certs anthonypillot/website
 
-#### HOST DOMAIN LIST ####
-# anthony-pillot.com www.anthony-pillot.com vps-51674b33.vps.ovh.net
-####
+############# HOST DOMAIN LIST ############
+# anthony-pillot.com www.anthony-pillot.com
+###########################################
