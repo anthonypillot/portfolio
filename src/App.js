869f8ed05732ed83254d3d998b1e9a5470cba0ae
Anthony Pillot <51130139+anthonypillot@users.noreply.github.com>
2021-06-03 22:04:05 +0200

    feat: creation of the application skeleton
    * feat: background, topbar and bottom bar with date time
    
    With navigation top-bar, logo created in SVG with Illustrator and center-box.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import TopBar from "./components/TopBar";
+import LoadingProgressBar from "./components/LoadingProgressBar";
+import "./App.css";
+import "./style/CenterBox.css";
+import "./style/Background.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar />
+
+      <div className="background"></div>
+      <div className="background background2"></div>
+      <div className="background background3"></div>
+
+      {/* Middle content */}
+
+      <div className="center-box">
+        <img
+          src="./img/logo_anthonypillotOS_black.svg"
+          alt="./img/logo_anthonypillotOS_black.svg"
+        ></img>
+        <h1>
+          Welcome to <br />
+          <span className="subtitle-center-box">
+            &#123; this.application &#125;
+          </span>
+        </h1>
+        <LoadingProgressBar />
+      </div>
+
+      {/* End of middle content */}
     </div>
   );
 }
